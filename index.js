bd41f8af457da4205b0de5647a42e4cfab8b3243
Nicola Marcacci Rossi <nicola@smartive.ch>
2024-06-18 16:05:24 +0200

    fix: Raw all

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ Structure of route:
 app.use(cors({ origin: '*' }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.text({ limit: '50mb' }));
-app.use(bodyParser.raw());
+app.use(bodyParser.raw({ type: '*/*' }));
 
 const smtpServer = new SMTPServer({
   authOptional: true,
