c89e5de287530baf93722cb661f00d9762f7eb9f
Nicola Marcacci Rossi <nicola@smartive.ch>
2024-06-18 15:56:41 +0200

    feat: Parsse xml

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ Structure of route:
 app.use(cors({ origin: '*' }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.text({ limit: '50mb' }));
+app.use(bodyParser.raw({ type: 'application/xml' }));
 
 const smtpServer = new SMTPServer({
   authOptional: true,
